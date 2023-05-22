0ba8bfb549a3e5304a6a89c5e525c427c2de52b8
ahmed okasha <ao1922302@gmail.com>
2023-05-22 03:55:50 +0300

    add favorite qoute

---
+++ lib/features/favorites_quotes/favorites_quotes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FavouriteQuote extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          Text('Favourite Qoutes'),
+        ],
+      ),
+    );
+  }
+}
